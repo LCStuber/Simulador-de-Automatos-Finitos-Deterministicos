8be03d7b933ad34f6d4be3c5db871e47f308c5f8
Leonardo Cazotto Stuber <leonardo_stu@outlook.com.br>
2023-08-28 18:46:41 -0300

    Atualização dos testes

--- test_automato.py
+++ test_automato.py
@@ -18,76 +18,76 @@ def automato4(cadeia:str):
 
 
 def teste1_cadeia1_verdadeira():
-    assert automato1("abbbaba") == True
+    assert automato1("000000") == True
 
 def teste1_cadeia2_verdadeira():
-    assert automato1("abbbababbababbabbbbb") == True
+    assert automato1("101110100") == True
 
 def teste1_cadeia3_verdadeira():
-    assert automato1("abbbabaababbababb") == True
+    assert automato1("01111001110") == True
 
 def teste1_cadeia1_falsa():
-    assert automato1("ababbbbbbbababababab") == False
+    assert automato1("0000001") == False
 
 def teste1_cadeia2_falsa():
-    assert automato1("abbbabaab") == False
+    assert automato1("1011101001") == False
 
 def teste1_cadeia3_falsa():
-    assert automato1("abbbabaababbabab") == False
+    assert automato1("011110011101") == False
 
 
 def teste2_cadeia1_verdadeira():
-    assert automato2("abbbaba") == True
+    assert automato2("000010") == True
 
 def teste2_cadeia2_verdadeira():
-    assert automato2("abbbababbababbabbbbb") == True
+    assert automato2("111111111111101111111111000") == True
 
 def teste2_cadeia3_verdadeira():
-    assert automato2("abbbabaababbababb") == True
+    assert automato2("10111111") == True
 
 def teste2_cadeia1_falsa():
-    assert automato2("ababbbbbbbababababab") == False
+    assert automato2("0000100") == False
 
 def teste2_cadeia2_falsa():
-    assert automato2("abbbabaab") == False
+    assert automato2("1111111111111011111111110000") == False
 
 def teste2_cadeia3_falsa():
-    assert automato2("abbbabaababbabab") == False
+    assert automato2("101111110") == False
 
 
 def teste3_cadeia1_verdadeira():
-    assert automato3("abbbaba") == True
+    assert automato3("011111111101") == True
 
 def teste3_cadeia2_verdadeira():
-    assert automato3("abbbababbababbabbbbb") == True
+    assert automato3("10000000000000001101") == True
 
 def teste3_cadeia3_verdadeira():
-    assert automato3("abbbabaababbababb") == True
+    assert automato3("11111111111111111101") == True
 
 def teste3_cadeia1_falsa():
-    assert automato3("ababbbbbbbababababab") == False
+    assert automato3("0111111111011") == False
 
 def teste3_cadeia2_falsa():
-    assert automato3("abbbabaab") == False
+    assert automato3("100000000000000011011") == False
 
 def teste3_cadeia3_falsa():
-    assert automato3("abbbabaababbabab") == False
+    assert automato3("111111111111111111011") == False
 
 
 def teste4_cadeia1_verdadeira():
-    assert automato4("abbbaba") == True
+    assert automato4("aaaaaaaaabbbbbbbbcba") == True
 
 def teste4_cadeia2_verdadeira():
-    assert automato4("abbbababbababbabbbbb") == True
+    assert automato4("cccaaaabbbcca") == True
 
 def teste4_cadeia3_verdadeira():
-    assert automato4("abbbabaababbababb") == True
+    assert automato4("aaababaacaaaabccccca") == True
 
 def teste4_cadeia1_falsa():
-    assert automato4("ababbbbbbbababababab") == False
+    assert automato4("aaaaaaaaabbbbbbbbcbac") == False
 
 def teste4_cadeia2_falsa():
-    assert automato4("abbbabaab") == False
+    assert automato4("cccaaaabbbccab") == False
 
 def teste4_cadeia3_falsa():
-    assert automato4("abbbabaababbabab") == False
+    assert automato4("aaababaacaaaabcccccac") == False
